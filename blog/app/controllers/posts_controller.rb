62d24112083e87dcec667ae9e716abba3e49a32c
Arthur <arthurbelyankove@gmail.com>
2019-06-08 15:44:28 +0600

    Added CRUD to post model

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -6,4 +6,38 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(title: params[:post][:title], text: params[:post][:text])
+
+    if @post.save
+      redirect_to posts_path, notice: "Созадан пост #{@post.id}"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(title: params[:post][:title], text: params[:post][:text])
+
+    if @post.save
+      redirect_to post_path(@post.id), notice: "Пост ##{@post.id} изменён"
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id]).destroy
+    redirect_to posts_path, notice: "Пост удалён"
+  end
 end
